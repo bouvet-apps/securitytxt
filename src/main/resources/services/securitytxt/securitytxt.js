fc6fbd92d82a8922b9e7cddaff66e6f6788eaa21
Jonas Lepsøy <jonas.lepsoy@bouvet.no>
2018-06-07 20:30:44 +0200

    Initial commit

---
+++ src/main/resources/services/securitytxt/securitytxt.js
@@ -0,0 +1,47 @@
+var libs = {
+    portal: require('/lib/xp/portal'),
+    util: require('/lib/enonic/util')
+};
+
+function handleGet() {
+
+    var siteConfig = libs.portal.getSiteConfig();
+    var securityTxt = "";
+
+    libs.util.data.forceArray(siteConfig.contactOptions).forEach(function(item) {
+        log.info(JSON.stringify(item, null, 2));
+        if (item._selected === 'url') {
+            securityTxt += 'Contact: ' + item[item._selected].url + '\n';
+        } else if (item._selected === 'tel') {
+            securityTxt += 'Contact: tel:' + item[item._selected].tel + '\n';
+        } else if (item._selected === 'email') {
+            securityTxt += 'Contact: mailto:' + item[item._selected].email+ '\n';
+        }
+    });
+
+    if (siteConfig.encryption !== '') {
+        securityTxt += 'Encryption: ' + libs.portal.pageUrl({type: 'absolute'}) + 'pgp-key.txt\n';
+    }
+
+    if (siteConfig.acknowledgements !== '') {
+        securityTxt += 'Acknowledgements: ' + siteConfig.acknowledgements + '\n';
+    }
+
+    if (siteConfig.policy !== '') {
+        securityTxt += 'Policy: ' + siteConfig.policy + '\n';
+    }
+
+    if (siteConfig.signature !== '') {
+        securityTxt += 'Signature: ' + libs.portal.pageUrl({type: 'absolute'}) + '.well-known/security.txt.sig\n';
+    }
+
+    if (siteConfig.hiring !== '') {
+        securityTxt += 'Hiring: ' + siteConfig.hiring + '\n';
+    }
+
+    return {
+        contentType: 'text/plain',
+        body: securityTxt
+    };
+}
+exports.get = handleGet;
---
+++ src/main/resources/services/securitytxt/signature.js
@@ -0,0 +1,21 @@
+var libs = {
+    portal: require('/lib/xp/portal'),
+    util: require('/lib/enonic/util')
+};
+
+function handleGet() {
+
+    var siteConfig = libs.portal.getSiteConfig();
+
+    if (siteConfig.signature !== '') {
+        return {
+            contentType: 'text/plain',
+            body: siteConfig.signature
+        };
+    } else {
+        return {
+            status: 404
+        };
+    }
+}
+exports.get = handleGet;
