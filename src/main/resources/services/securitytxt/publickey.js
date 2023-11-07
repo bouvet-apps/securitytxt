c1ac41023690f1dd1cc86000dfa02f30a901acc9
PeterSund <56296549+PeterSund@users.noreply.github.com>
2023-11-07 15:57:31 +0100

    feature/signature: Finished textarea option, formatted xml, added lang, canonical + comments. Updated readme

--- src/main/resources/services/securitytxt/publickey.js
+++ src/main/resources/services/securitytxt/publickey.js
@@ -1,21 +1,20 @@
 const libs = {
-    portal: require('/lib/xp/portal'),
-    util: require('/lib/util')
+  portal: require("/lib/xp/portal"),
+  util: require("/lib/util"),
 };
 
 function handleGet() {
+  const siteConfig = libs.portal.getSiteConfig();
 
-    const siteConfig = libs.portal.getSiteConfig();
-
-    if (siteConfig.encryption !== '') {
-        return {
-            contentType: 'text/plain',
-            body: siteConfig.encryption
-        };
-    } else {
-        return {
-            status: 404
-        };
-    }
+  if (siteConfig.option.fields.encryption !== "") {
+    return {
+      contentType: "text/plain",
+      body: siteConfig.option.fields.encryption !== "",
+    };
+  } else {
+    return {
+      status: 404,
+    };
+  }
 }
 exports.get = handleGet;
--- src/main/resources/services/securitytxt/securitytxt.js
+++ src/main/resources/services/securitytxt/securitytxt.js
@@ -1,88 +1,97 @@
 const libs = {
-    portal: require('/lib/xp/portal'),
-    util: require('/lib/util')
+  portal: require("/lib/xp/portal"),
+  util: require("/lib/util"),
 };
 
-// Takes the old way of configuring app, puts them in the optionset for unsigned configuration and removes the old config.
+// Takes the old way of configuring app, puts them in the optionset for textarea configuration and removes the old config.
 function createNewConfigOptions(siteConfig) {
-    for (let key in siteConfig) {
-        if (siteConfig.hasOwnProperty(key)) {
-            siteConfig.signedOptionSet.unsigned = siteConfig[key];
-            delete siteConfig.key;
-        }
+  for (let key in siteConfig) {
+    if (siteConfig.hasOwnProperty(key)) {
+      siteConfig.options.fields = siteConfig[key];
+      delete siteConfig.key;
     }
-    return siteConfig;
+  }
+  return siteConfig;
 }
 
-// Creates the securityTxt from the unsigned options, same way as done in previous versions of app.
-function createUnsignedSecurityTxt(siteConfig) {
-    let securityTxt = ""
+// Creates the securityTxt from the textarea options, same way as done in previous versions of app.
+function createtextareaSecurityTxt(siteConfig) {
+  let securityTxt = "";
 
-    libs.util.data.forceArray(siteConfig.contactOptions).forEach(function (item) {
-        if (item._selected === 'url' && item[item._selected].url) {
-            securityTxt += 'Contact: ' + item[item._selected].url + '\n';
-        } else if (item._selected === 'tel' && item[item._selected].tel) {
-            securityTxt += 'Contact: tel:' + item[item._selected].tel + '\n';
-        } else if (item._selected === 'email' && item[item._selected].email) {
-            securityTxt += 'Contact: mailto:' + item[item._selected].email + '\n';
-        }
-    });
+  if (siteConfig.comments) {
+    securityTxt += siteConfig.comments + "\n";
+  }
 
-    if (siteConfig.encryption) {
-        securityTxt += 'Encryption: ' + libs.portal.pageUrl({ id: libs.portal.getSite()._id,type: 'absolute' }) + 'pgp-key.txt\n';
+  libs.util.data.forceArray(siteConfig.contactOptions).forEach(function (item) {
+    if (item._selected === "url" && item[item._selected].url) {
+      securityTxt += "Contact: " + item[item._selected].url + "\n";
+    } else if (item._selected === "tel" && item[item._selected].tel) {
+      securityTxt += "Contact: tel:" + item[item._selected].tel + "\n";
+    } else if (item._selected === "email" && item[item._selected].email) {
+      securityTxt += "Contact: mailto:" + item[item._selected].email + "\n";
     }
+  });
 
-    if (siteConfig.acknowledgements) {
-        securityTxt += 'Acknowledgements: ' + siteConfig.acknowledgements + '\n';
-    }
+  if (siteConfig.displayCanonical) {
+    const defaultUrl = libs.portal.pageUrl({id: libs.portal.getSite()._id, type: "absolute",});
+    securityTxt += "Canonical: " + defaultUrl + ".well-known/security.txt" + "\n" + 
+    "Canonical: " + defaultUrl + "security.txt" + "\n";
+  }
 
-    if (siteConfig.policy) {
-        securityTxt += 'Policy: ' + siteConfig.policy + '\n';
-    }
+  if (siteConfig.encryption) {
+    securityTxt += "Encryption: " + libs.portal.pageUrl({ id: libs.portal.getSite()._id, type: "absolute" }) + "pgp-key.txt\n";
+  }
 
-    if (siteConfig.hiring) {
-        securityTxt += 'Hiring: ' + siteConfig.hiring + '\n';
-    }
+  if (siteConfig.acknowledgements) {
+    securityTxt += "Acknowledgements: " + siteConfig.acknowledgements + "\n";
+  }
 
-    if (siteConfig.expiresOptions &&
-        siteConfig.expiresOptions.static &&
-        siteConfig.expiresOptions.static.date) {
+  if (siteConfig.policy) {
+    securityTxt += "Policy: " + siteConfig.policy + "\n";
+  }
 
-        const expiryDate = new Date(siteConfig.expiresOptions.static.date);
+  if (siteConfig.hiring) {
+    securityTxt += "Hiring: " + siteConfig.hiring + "\n";
+  }
 
-        securityTxt += 'Expires: ' + expiryDate.toISOString() + '\n';
+  if (siteConfig.preferredLanguages) {
+    securityTxt +=
+      "Preferred-Languages: " + siteConfig.preferredLanguages + "\n";
+  }
 
-    } else {
-        // Expiry date is mandatory. Default to dynamic expiry date if options are missing.
-        const expiryDate = new Date();
-        expiryDate.setDate(expiryDate.getDate() + 60);
+  if (siteConfig.expiresOptions && siteConfig.expiresOptions.static && siteConfig.expiresOptions.static.date) {
+    const expiryDate = new Date(siteConfig.expiresOptions.static.date);
+    securityTxt += "Expires: " + expiryDate.toISOString() + "\n";
+  } else {
+    // Expiry date is mandatory. Default to dynamic expiry date if options are missing.
+    const expiryDate = new Date();
+    expiryDate.setDate(expiryDate.getDate() + 60);
+    securityTxt += "Expires: " + expiryDate.toISOString() + "\n";
+  }
 
-        securityTxt += 'Expires: ' + expiryDate.toISOString() + '\n';
-    }
-
-    return securityTxt;
+  return securityTxt;
 }
 
 function handleGet() {
-    let siteConfig = libs.portal.getSiteConfig();
+  let siteConfig = libs.portal.getSiteConfig();
 
-    let securityTxt = "";
+  let securityTxt = "";
 
-    // Check if new config per version 2.2.0 is not present and if any of the old configuration options are available.
-    if(!siteConfig.signedOptionSet && (siteConfig.contactOptions || siteConfig.encryption || siteConfig.hiring || siteConfig.policy || siteConfig.expiresOptions)) {
-        siteConfig = createNewConfigOptions(siteConfig);
-    }
+  // Check if new config per version 2.2.0 is not present and if any of the old configuration options are available.
+  if (!siteConfig.options && (siteConfig.contactOptions || siteConfig.encryption || siteConfig.hiring || siteConfig.policy || siteConfig.expiresOptions)) {
+    siteConfig = createNewConfigOptions(siteConfig);
+  }
 
-    if (siteConfig.signedOptionSet.signed) {
-        securityTxt = siteConfig.signedOptionSet.signed.signedText;
-    } else {
-        const unsignedConfig = siteConfig.signedOptionSet.unsigned;
-        securityTxt = createUnsignedSecurityTxt(unsignedConfig);
-    }
+  if (siteConfig.options.textarea) {
+    securityTxt = siteConfig.options.textarea.text;
+  } else {
+    const fieldsConfig = siteConfig.options.fields;
+    securityTxt = createtextareaSecurityTxt(fieldsConfig);
+  }
 
-    return {
-        contentType: 'text/plain',
-        body: securityTxt
-    };
+  return {
+    contentType: "text/plain",
+    body: securityTxt,
+  };
 }
 exports.get = handleGet;
