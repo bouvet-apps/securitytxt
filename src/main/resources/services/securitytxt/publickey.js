2b8327d294ba330ac1cb7590ff137fce67553029
Roeland-Bouvet <roeland.gallein@bouvet.no>
2024-10-18 09:17:04 +0200

    feature/BOUENO-119: added public key input for the textarea option, fixed bug and imroved helper texts

--- src/main/resources/services/securitytxt/publickey.js
+++ src/main/resources/services/securitytxt/publickey.js
@@ -6,15 +6,30 @@ const libs = {
 function handleGet() {
   const siteConfig = libs.portal.getSiteConfig();
 
-  if (siteConfig.option.fields.encryption !== "") {
+  if (siteConfig?.options?._selected === "textarea" && siteConfig?.options?.textarea?.encryption) {
     return {
       contentType: 'text/plain',
-      body: siteConfig.option.fields.encryption !== "",
-    };
-  } else {
-    return {
-      status: 404
+      body: siteConfig.options.textarea.encryption,
     };
   }
+
+  if (siteConfig?.options?._selected === "fields" && siteConfig?.options?.fields?.encryption) {
+    return {
+      contentType: 'text/plain',
+      body: siteConfig.options.fields.encryption,
+    };
+  }
+
+  // Fallback for older config (in case people update the app but forget to set the new configuration)
+  if (!siteConfig?.options?._selected && siteConfig?.encryption) {
+    return {
+      contentType: 'text/plain',
+      body: siteConfig.encryption,
+    };
+  }
+
+  return {
+    status: 404
+  };
 }
 exports.get = handleGet;
